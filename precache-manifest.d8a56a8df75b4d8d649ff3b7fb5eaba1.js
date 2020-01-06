cece0eb80899916c4b2598da641a8f712311167b
Mario Jackson <mario@Marios-Dark-Mac.local>
2020-01-06 01:18:56 +0100

    Updates

---
+++ precache-manifest.d8a56a8df75b4d8d649ff3b7fb5eaba1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ba9a7dc67957bc52de23ad39cb3771e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "45a7fb1acd6ee7b1f78e",
+    "url": "/monsters-rolodex/static/css/main.2da39f46.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "45a7fb1acd6ee7b1f78e",
+    "url": "/monsters-rolodex/static/js/main.f8c761a6.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
